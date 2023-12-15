b44ea40b7eb629c5b14a697d223daef1aa7fd673
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-12-15 09:49:52 -0600

    happy

--- MASH/ViewController5.swift
+++ MASH/ViewController5.swift
@@ -13,11 +13,72 @@ class ViewController5: UIViewController {
     var i2 = 0
     var i3 = 0
     var i4 = 0
+    var s = ""
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        while i1 < 16
+        {
+    if i1 < 4
+    {
+        AppData.answerQ1Array.append(AppData.answerArray[i1])
+        print("yay")
+    }else if i1 < 8{
+        AppData.answerQ2Array.append(AppData.answerArray[i1])
+        print("yay2")
+    }else if i1 < 12{
+        AppData.answerQ3Array.append(AppData.answerArray[i1])
+        print("yay3")
+    }else{
+        AppData.answerQ4Array.append(AppData.answerArray[i1])
+        print("yay4")
+    }
+            i1 = i1 + 1
+        }
+        
+        
+        //putting in label
+        s = "\(s) \n\(AppData.quesArray[0])"
+        labelOutlet.text = "\(s)"
+        for blah in AppData.answerQ1Array
+        {
+    
+            s = "\(s) \n\(blah)"
+            labelOutlet.text = "\(s)"
+        }
        
+        s = "\(s) \n\(AppData.quesArray[1])"
+        labelOutlet.text = "\(s)"
+        
+        for blah in AppData.answerQ2Array
+        {
+           
+            s = "\(s) \n\(blah)"
+            labelOutlet.text = "\(s)"
+        }
+        
+        
+        s = "\(s) \n\(AppData.quesArray[2])"
+        labelOutlet.text = "\(s)"
+        for blah in AppData.answerQ3Array
+        {
+           
+            s = "\(s) \n\(blah)"
+            labelOutlet.text = "\(s)"
+        }
+        s = "\(s) \n\(AppData.quesArray[3])"
+        labelOutlet.text = "\(s)"
+        for blah in AppData.answerQ4Array
+        {
+           
+            s = "\(s) \n\(blah)"
+            labelOutlet.text = "\(s)"
+        }
+        
+        
     }
     
 
--- ViewController3.swift
+++ ViewController3.swift
@@ -9,10 +9,66 @@ import UIKit
 
 class ViewController3: UIViewController {
 
+    @IBOutlet weak var label1Outlet: UILabel!
+    
+    @IBOutlet weak var label2Outlet: UILabel!
+    
+    @IBOutlet weak var label3Outlet: UILabel!
+    
+    @IBOutlet weak var label4Outlet: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //drive
+        AppData.answerQ1Array.append("Bike")
+        AppData.answerQ1Array.append("Lambo")
+        AppData.answerQ1Array.append("Train")
+        AppData.answerQ1Array.append("Kia Sol")
+        //live
+        AppData.answerQ2Array.append("Mansion")
+        AppData.answerQ2Array.append("Apartment")
+        AppData.answerQ2Array.append("Shed")
+        AppData.answerQ2Array.append("House")
+        //pet
+        AppData.answerQ3Array.append("Rat")
+        AppData.answerQ3Array.append("Golden Retriever")
+        AppData.answerQ3Array.append("Pitbull")
+        AppData.answerQ3Array.append("Tarantula")
+        //marry
+        AppData.answerQ4Array.append("The Rock")
+        AppData.answerQ4Array.append("Santa Claus/Mrs. Claus")
+        AppData.answerQ4Array.append("The person sitting next to you")
+        AppData.answerQ4Array.append("Josh")
+        
+        //printing
+        var s = ""
+    
+        for blah in AppData.answerQ1Array
+        {
+            s = "\(s)\(blah)\n"
+            label1Outlet.text = "\(s)"
+        }
+        var s2 = ""
+        for blah in AppData.answerQ2Array
+        {
+            s2 = "\(s2)\(blah)\n"
+            label2Outlet.text = "\(s2)"
+        }
+        var s3 = ""
+        for blah in AppData.answerQ3Array
+        {
+            s3 = "\(s3)\(blah)\n"
+            label3Outlet.text = "\(s3)"
+        }
+        var s4 = ""
+        for blah in AppData.answerQ4Array
+        {
+            s4 = "\(s4)\(blah)\n"
+            label4Outlet.text = "\(s4)"
+        }
+        
     }
     
 
